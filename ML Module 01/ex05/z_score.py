47cf12a99f124a12efd2c26d603e8522352d3ffe
Sungyong Cho <to.sungyongcho@gmail.com>
2023-05-17 15:43:45 +0200

    chore: update ex05, ex06 with eval

--- ML Module 01/ex05/z_score.py
+++ ML Module 01/ex05/z_score.py
@@ -1,6 +1,6 @@
 import numpy
 from TinyStatistician import TinyStatistician as TS
-
+from sklearn.preprocessing import StandardScaler
 
 def zscore(x):
     """Computes the normalized version of a non-empty numpy.ndarray using the z-score standardization.
@@ -18,10 +18,34 @@ def zscore(x):
     result = (x - aaa.mean(x)) / aaa.std(x)
     return result
 
+def eval():
+    X = numpy.array([0, 15, -9, 7, 12, 3, -21])
+    print(zscore(X))
+    scaler = StandardScaler()
+    X_reshaped = X.reshape((-1, 1))  # Reshape X to a 2D array
+    scaler.fit(X_reshaped)
+    print("==========x============")
+    print("--------scikit-learn-----")
+    print(scaler.transform(X_reshaped))
+    print("--------mine-----")
+    print(zscore(X))
+    print("==========x============")
+    Y = numpy.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
+    Y_reshaped = Y.reshape((-1, 1))
+    scaler.fit(Y_reshaped)
+
+    print("==========y============")
+    print("--------scikit-learn-----")
+    print(scaler.transform(Y_reshaped))
+    print("--------mine-----")
+    print(zscore(Y))
+    print("==========y============")
 
 if __name__ == "__main__":
     X = numpy.array([0, 15, -9, 7, 12, 3, -21])
-    print(zscore(X))
+    # print(zscore(X))
     Y = numpy.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
-    print(zscore(Y))
+    # print(zscore(Y))
     # print(result)
+
+    eval()
--- ML Module 01/ex06/minmax.py
+++ ML Module 01/ex06/minmax.py
@@ -1,5 +1,6 @@
 import numpy as np
 from TinyStatistician import TinyStatistician as TS
+from sklearn.preprocessing import MinMaxScaler
 
 
 def minmax(x):
@@ -23,17 +24,41 @@ def minmax(x):
 
     return (x - min_x) / (max_x - min_x)
 
+def eval():
+    X = np.array([0, 15, -9, 7, 12, 3, -21]).reshape((-1, 1))
+    scaler = MinMaxScaler()
+    X_reshaped = X.reshape((-1, 1))  # Reshape X to a 2D array
+    scaler.fit(X_reshaped)
+    print("==========x============")
+    print("--------scikit-learn-----")
+    print(scaler.transform(X_reshaped))
+    print("--------mine-----")
+    print(minmax(X))
+    print("==========x============")
+
+    Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
+    Y_reshaped = Y.reshape((-1, 1))  # Reshape X to a 2D array
+    scaler.fit(Y_reshaped)
+    print("==========Y============")
+    print("--------scikit-learn-----")
+    print(scaler.transform(Y_reshaped))
+    print("--------mine-----")
+    print(minmax(Y))
+    print("==========Y============")
+
 
 if __name__ == "__main__":
     # Example 1:
     X = np.array([0, 15, -9, 7, 12, 3, -21]).reshape((-1, 1))
-    print(minmax(X))
+    # print(minmax(X))
     # # Output:
     # array([0.58333333, 1. , 0.33333333, 0.77777778, 0.91666667,
     # 0.66666667, 0. ])
     # Example 2:
     Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
-    print(minmax(Y))
+    # print(minmax(Y))
     # # Output:
     # array([0.63636364, 1. , 0.18181818, 0.72727273, 0.93939394,
     # 0.6969697 , 0. ])
+
+    eval()
